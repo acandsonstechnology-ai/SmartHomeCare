05e508bb04bd44e49faed9c301843a1fa5380a24
vignesh5142 <vigneshm5142@gmail.com>
2026-02-06 21:56:18 +0530

    react navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './routes';
 
 function App() {
   return (
     <div className="App">
-<h1>hello </h1>
+      <Navigation />
     </div>
   );
 }
---
+++ src/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import LandingPage from './Pages/Landing/landingPage'
+
+
+function Navigation() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<LandingPage />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Navigation
